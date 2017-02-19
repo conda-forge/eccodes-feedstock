e717cc943b420ad86c17b7e6f563017313746ace
ocefpaf <ocefpaf@gmail.com>
2017-02-19 14:56:47 -0300

    win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,11 +6,12 @@ set PYTHON=
 :: Shared.
 cmake -G "NMake Makefiles" ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-      -D ENABLE_JPG=1 \
-      -D ENABLE_NETCDF=1 \
-      -D ENABLE_PNG=1 \
-      -D ENABLE_PYTHON=0 \
-      -D ENABLE_FORTRAN=0 \
+      -D ENABLE_JPG=1 ^
+      -D ENABLE_NETCDF=1 ^
+      -D ENABLE_PNG=1 ^
+      -D ENABLE_PYTHON=0 ^
+      -D ENABLE_FORTRAN=0 ^
+      -D DISABLE_OS_CHECK=ON ^
       %SRC_DIR%
 if errorlevel 1 exit 1
 
