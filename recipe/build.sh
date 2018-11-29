a31c027a4b11cc4be02ec99f8826ed9d4353852c
Isuru Fernando <isuruf@gmail.com>
2018-11-28 20:12:35 -0600

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,7 +8,7 @@ export WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export BUILD_OUTPUT=$WORKDIR/build.out
 
 if [[ "$c_compiler" == "gcc" ]]; then
-  export LDFLAGS="${LDFLAGS} -L${BUILD_PREFIX}/${HOST}/sysroot/lib"
+  export PATH="${PATH}:${BUILD_PREFIX}/${HOST}/sysroot/lib"
 fi
 
 touch $BUILD_OUTPUT
