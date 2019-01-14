1c83f48846a37a3af16bcfec6931885a9b74f67e
Daniel Tipping <daniel@oldreliable.tech>
2019-01-14 10:30:36 +0000

    Experimental: Add Windows support

---
+++ recipe/bld.bat
@@ -0,0 +1,30 @@
+mkdir build
+cd build
+
+set CFLAGS=
+set CXXFLAGS=
+
+cmake -G "NMake Makefiles" ^
+      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -D CMAKE_BUILD_TYPE=Release ^
+      -D ENABLE_FORTRAN=0 ^
+      -D ENABLE_PYTHON=0 ^
+      -D ENABLE_NETCDF=1 ^
+      -D ENABLE_JPG=1 ^
+      -D IEEE_LE=1 ^
+      -D ENABLE_MEMFS=0 ^
+      -D ENABLE_EXTRA_TESTS=OFF ^
+      ..
+if errorlevel 1 exit 1
+
+nmake
+if errorlevel 1 exit 1
+
+:: so tests can find eccodes.dll
+set PATH=%PATH%;%SRC_DIR%\eccodes\build\bin
+
+ctest
+if errorlevel 1 exit 1
+
+nmake install
+if errorlevel 1 exit 1
