1ef507f9ef2e93fda07b534b7bd72a45e925215e
Filipe Fernandes <ocefpaf@gmail.com>
2019-03-27 12:31:34 -0300

    Azure does not need this workaround

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 
 
-set -e # Abort on error.
-
-export PING_SLEEP=30s
-export WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export BUILD_OUTPUT=$WORKDIR/build.out
+set -e
 
 if [[ "$c_compiler" == "gcc" ]]; then
   export PATH="${PATH}:${BUILD_PREFIX}/${HOST}/sysroot/usr/lib"
 fi
-
-touch $BUILD_OUTPUT
 
 if [[ $(uname) == Linux ]]; then
     export CC=$(basename ${CC})
@@ -19,24 +13,6 @@ if [[ $(uname) == Linux ]]; then
     export FC=$(basename ${FC})
 fi
 
-dump_output() {
-   echo Tailing the last 500 lines of output:
-   tail -500 $BUILD_OUTPUT
-}
-error_handler() {
-  echo ERROR: An error was encountered with the build.
-  dump_output
-  exit 1
-}
-
-# If an error occurs, run our error handler to output a tail of the build.
-trap 'error_handler' ERR
-
-# Set up a repeating loop to send some output to Travis.
-bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
-PING_LOOP_PID=$!
-
-## START BUILD
 if [[ $(uname) == Darwin ]]; then
   export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
   export FFLAGS="-isysroot $CONDA_BUILD_SYSROOT $FFLAGS"
@@ -63,23 +39,17 @@ cmake $src_dir \
          -DENABLE_AEC=1 \
          -DREPLACE_TPL_ABSOLUTE_PATHS=$REPLACE_TPL_ABSOLUTE_PATHS
 
-make -j $CPU_COUNT VERBOSE=1 >> $BUILD_OUTPUT 2>&1
+make -j $CPU_COUNT VERBOSE=1
 export ECCODES_TEST_VERBOSE_OUTPUT=1
 eval ${LIBRARY_SEARCH_VAR}=$PREFIX/lib
 
-ctest --output-on-failure -j $CPU_COUNT >> $BUILD_OUTPUT 2>&1
+ctest --output-on-failure -j $CPU_COUNT
 
-make install >> $BUILD_OUTPUT 2>&1
+make install
 
 # Replace any leaked build env
 if [[ $(uname) == Linux ]]; then
-    find $PREFIX/include -type f -print0 | xargs -0 sed -i "s@${BUILD_PREFIX}@${PREFIX}@g" >> $BUILD_OUTPUT 2>&1
-    find $PREFIX/lib/pkgconfig -type f -print0 | xargs -0 sed -i "s@${BUILD_PREFIX}@${PREFIX}@g" >> $BUILD_OUTPUT 2>&1
-    find $PREFIX/share/eccodes/cmake -type f -print0 | xargs -0 sed -i "s@${BUILD_PREFIX}@${PREFIX}@g" >> $BUILD_OUTPUT 2>&1
+    find $PREFIX/include -type f -print0 | xargs -0 sed -i "s@${BUILD_PREFIX}@${PREFIX}@g"
+    find $PREFIX/lib/pkgconfig -type f -print0 | xargs -0 sed -i "s@${BUILD_PREFIX}@${PREFIX}@g"
+    find $PREFIX/share/eccodes/cmake -type f -print0 | xargs -0 sed -i "s@${BUILD_PREFIX}@${PREFIX}@g"
 fi
-
-# The build finished without returning an error so dump a tail of the output.
-dump_output
-
-# Nicely terminate the ping output loop.
-kill $PING_LOOP_PID
