25657f221e5109807c817c57cad9de619c861c14
Ryan May <rmay@ucar.edu>
2019-11-19 01:23:20 -0700

    Openjpeg (#92)
    * Switch from jasper to openjpeg
    * MNT: Re-rendered with conda-build 3.18.11, conda-smithy 3.6.2, and conda-forge-pinning 2019.11.13
    * Bump build number
    * Adjust path to fix running tests
    * Enable PNG on windows
    * MNT: Re-rendered with conda-build 3.18.11, conda-smithy 3.6.2, and conda-forge-pinning 2019.11.14
    * Try to fix PATH for running tests

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,10 +6,13 @@ set CXXFLAGS=
 cmake -G "NMake Makefiles" ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D CMAKE_BUILD_TYPE=Release ^
+      -D OPENJPEG_INCLUDE_DIR=%LIBRARY_INC%\openjpeg-2.3 ^
+      -D OPENJPEG_PATH=%LIBRARY_PREFIX% ^
       -D ENABLE_FORTRAN=0 ^
       -D ENABLE_PYTHON=0 ^
       -D ENABLE_NETCDF=1 ^
       -D ENABLE_JPG=1 ^
+      -D ENABLE_PNG=1 ^
       -D IEEE_LE=1 ^
       -D ENABLE_MEMFS=0 ^
       -D ENABLE_EXTRA_TESTS=OFF ^
@@ -20,7 +23,7 @@ nmake
 if errorlevel 1 exit 1
 
 :: so tests can find eccodes.dll
-set PATH=%PATH%;%SRC_DIR%\build\bin
+set PATH=%PATH%;%LIBRARY_BIN%;%SRC_DIR%\build\bin
 
 ctest --output-on-failure
 if errorlevel 1 exit 1
