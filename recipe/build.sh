ffd945785f3244fe57a76fc12d1d42183e3f1f75
Shahram Najm <masn@ecmwf.int>
2020-10-16 18:15:31 +0100

    Final version. CMAKE_FIND_ROOT_PATH_MODE_INCLUDE=BOTH fixed it

--- recipe/build.sh
+++ recipe/build.sh
@@ -21,9 +21,6 @@ export CFLAGS="$CFLAGS -fPIC -I$PREFIX/include"
 
 mkdir ../build && cd ../build
 
-env | sort
-ls -l $PREFIX/lib/
-
 cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
       -D CMAKE_BUILD_TYPE=Release \
       -D ENABLE_JPG=1 \
